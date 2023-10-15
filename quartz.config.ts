9cc342d412f36259bd77bcdf6d3261a9c9afae94
KaiDesigns <kaidesign1993@gmail.com>
2023-10-15 09:52:28 +0800

    google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: 'google', tagID: <G-YXMKXXDNCS>,
     },
     baseUrl: "kaibrain.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
